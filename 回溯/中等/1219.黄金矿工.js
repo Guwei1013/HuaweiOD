74f17992687bdfab40cf3dc9965bc41c0fd0cd2f
guwei <1604125861@qq.com>
2023-04-03 10:04:59 +0800

    代码优化，1219，2023.4.3

--- 回溯/中等/1219.黄金矿工.js
+++ 回溯/中等/1219.黄金矿工.js
@@ -3,29 +3,30 @@ var getMaximumGold = function(grid) {
     let maxValue = 0
 
     function backtrack(y,x,row,col,val,visited) {
-        visited.add(`${y},${x}`)
+        const origin = grid[y][x]
+        grid[y][x] = 0
         let curMax = val 
 
         /* 上 */
-        if(y-1>=0 && grid[y-1][x] !== 0 && !visited.has(`${y-1},${x}`)) {
+        if(y-1>=0 && grid[y-1][x] !== 0) {
             curMax = Math.max(curMax,backtrack(y-1,x,row,col,val+grid[y-1][x],visited))
         }
         /* 下 */
-        if(y+1<row && grid[y+1][x] !== 0 && !visited.has(`${y+1},${x}`)) {
+        if(y+1<row && grid[y+1][x] !== 0) {
             curMax = Math.max(curMax,backtrack(y+1,x,row,col,val+grid[y+1][x],visited))
         }
 
         /* 左 */
-        if(x-1>=0 && grid[y][x-1] !== 0 && !visited.has(`${y},${x-1}`)) {
+        if(x-1>=0 && grid[y][x-1] !== 0) {
             curMax = Math.max(curMax,backtrack(y,x-1,row,col,val+grid[y][x-1],visited))
         }
 
         /* 右 */
-        if(x+1<col && grid[y][x+1] !== 0 && !visited.has(`${y},${x+1}`)) {
+        if(x+1<col && grid[y][x+1] !== 0) {
             curMax = Math.max(curMax,backtrack(y,x+1,row,col,val+grid[y][x+1],visited))
         }
 
-        visited.delete(`${y},${x}`)
+        grid[y][x] = origin
         return curMax
     }
 
