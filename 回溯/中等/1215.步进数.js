5c05cb1ab44f1cacc1089b871743df91d57696a6
guwei <1604125861@qq.com>
2023-05-05 21:43:34 +0800

    代码优化

--- 回溯/中等/1215.步进数.js
+++ 回溯/中等/1215.步进数.js
@@ -8,13 +8,12 @@ var countSteppingNumbers = function(low, high) {
   function dfs(sum, last) {
     if(sum >= low && sum <= high) {
       result.push(sum)
-    } else if(sum > high) {
-      return
     }
 
     for(let i=0; i<=9; i++) {
-      if(last === i+1 || last === i-1) {
-        dfs(sum*10+i,i)
+      const val = sum*10+i
+      if((last === i+1 || last === i-1) && val <= high) {
+        dfs(val,i)
       }
     }
   }
@@ -24,4 +23,30 @@ var countSteppingNumbers = function(low, high) {
   }
 
   return result.sort((a,b) => a-b)
+};
+
+
+/**
+ * 广度优先遍历
+ * @param {number} low
+ * @param {number} high
+ * @return {number[]}
+ */
+var countSteppingNumbers = function(low, high) {
+  const result = low > 0 ? [] : [0]
+  const path = [[1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7],[8,8],[9,9]]
+  while (path.length) {
+    const [sum, last] = path.shift()
+    if (sum >= low && sum <= high) {
+      result.push(sum)
+    }
+
+    for (let i=0; i<=9; i++) {
+      const val = i + sum*10
+      if((last === i+1 || last === i-1) && val <= hi) {
+        path.push([i + sum*10, i])
+      }
+    }
+  } 
+  return result
 };
