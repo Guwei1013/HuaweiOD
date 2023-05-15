9c43bfe24a24d8b4d77b6892a4e2541abc5de12d
guwei <1604125861@qq.com>
2023-05-15 22:16:28 +0800

    代码优化

--- 栈/简单/1021.删除最外层的括号.js
+++ 栈/简单/1021.删除最外层的括号.js
@@ -2,27 +2,20 @@
  * @param {string} s
  * @return {string}
  */
-var removeOuterParentheses = function (s) {
-  let result = "",
-    stack = [],
-    left = 0,
-    right = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === "(") {
-      left++;
-    } else {
-      right++;
-    }
-    stack.push(s[i]);
+var removeOuterParentheses = function(s) {
+  let count = 0,result = ''
+  for (let i=0; i<s.length; i++) {
+      if (s[i] === ')') {
+          count--
+      } 
 
-    if (left === right) {
-      for (let i = 1; i < stack.length - 1; i++) {
-        result += stack[i];
+      if (count) {
+          result += s[i]
       }
-      stack = [];
-      left = 0;
-      right = 0;
-    }
+
+      if (s[i] === '(') {
+          count++
+      }
   }
-  return result;
+  return result
 };
