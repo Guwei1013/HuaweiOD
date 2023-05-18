f257ce5ddbfd49a72b15dce58702339e6f1e86b3
guwei <1604125861@qq.com>
2023-05-18 22:01:05 +0800

    2023.5.18 杨辉三角

---
+++ 动态规划/简单/118.杨辉三角.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  const result = [1];
+  for (let i = 0; i < numRows; i++) {
+    result[i] = new Array(i + 1).fill(1);
+    for (let j = 1; j < i; j++) {
+      result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+    }
+  }
+  return result;
+};
