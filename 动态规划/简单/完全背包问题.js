51f25282ad7d3e28fb02b8a8c93902e5d955b739
guwei <1604125861@qq.com>
2023-05-24 00:07:13 +0800

    2023.5.24 代码优化

--- 动态规划/简单/完全背包问题.js
+++ 动态规划/简单/完全背包问题.js
@@ -1,15 +1,9 @@
-function fn(N, V, goods) {
-  const dp = new Array(V + 1).fill(0).map((val) => new Array(N + 1).fill(0));
-  console.log(dp)
-  for (let i = 1; i <= V; i++) {
-    for (let j = 1; j <= N; j++) {
-      const [weight, value] = goods[j - 1];
-      if (i < weight) {
-        dp[i][j] = dp[i][j-1]
-      } else {
-        dp[i][j] = Math.max(dp[i][j - 1], dp[i - weight][j] + value);
-      }
+function fn(N, V, weight, value) {
+  const dp = new Array(V + 1).fill(0)
+  for (let i = 1; i <= N; i++) {
+    for (let j = weight[i]; j <= V; j++) {
+      dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[j])
     }
   }
-  return dp[V][N];
+  return dp[V];
 }
