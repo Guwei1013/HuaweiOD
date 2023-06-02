f0f9978338980e5601ca682ba4da9044284c0658
guwei <1604125861@qq.com>
2023-06-02 21:08:35 +0800

    2023.6.2 二分

---
+++ 二分/中等/2517.礼盒最大甜蜜度.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} price
+ * @param {number} k
+ * @return {number}
+ */
+var maximumTastiness = function (price, k) {
+  price.sort((a, b) => a - b);
+  const len = price.length;
+  let min = 0,
+    max = price[len - 1] - price[0];
+  while (min <= max) {
+    let gap = Math.ceil((min + max) / 2);
+    let num = 1;
+    for (let left = 0, right = 1; right < len; right++) {
+      if (price[right] - price[left] >= gap) {
+        num++;
+        left = right;
+      }
+    }
+
+    if (num >= k) {
+      min = gap + 1;
+    } else if (num < k) {
+      max = gap - 1;
+    }
+  }
+  return min - 1;
+};
